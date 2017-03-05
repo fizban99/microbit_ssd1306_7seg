a77e956f12560db7c8f5e3e7cb65e755f034571b
fizban99 <angelivanm@gmail.com>
2017-03-05 18:55:31 +0100

    timer v1

--- samples/kitchen_timer/main.py
+++ samples/kitchen_timer/main.py
@@ -1,31 +1,61 @@
 from ssd1306_7seg import initialize, disp_number, blink
 from microbit import running_time, sleep, button_a, button_b
+from music import play
+
+
+def wait_unpressed():
+    while button_a.is_pressed() or button_b.is_pressed():
+        sleep(100)
+
+
+def wait_unpressed2(button1, button2):
+    i = 0
+    while i < 300 and button1.is_pressed() and not button2.is_pressed():
+        sleep(10)
+        i = i + 10
+
 
 initialize(1, 1)
-minutes, seconds, timer = 0, 0, 0
+minutes, seconds, timer, offset, increments = 0, 0, 0, 0, True
 while True:
     while not (button_a.is_pressed() and button_b.is_pressed()):
-        i = 0
         if button_a.is_pressed() and not button_b.is_pressed():
-            while i < 300 and button_a.is_pressed() and not button_b.is_pressed():
-                sleep(10)
-                i = i + 10
+            wait_unpressed2(button_a, button_b)
             if not button_b.is_pressed():
                 minutes = (minutes + 1) % 100
+                play(["B6:1"], wait=False)
+                increments, offset = False, 0
                 disp_number(minutes * 100 + seconds)
         elif button_b.is_pressed() and not button_a.is_pressed():
-            while i < 300 and button_b.is_pressed() and not button_a.is_pressed():
-                sleep(10)
-                i = i + 10
+            wait_unpressed2(button_b, button_a)
             if not button_a.is_pressed():
                 seconds = (seconds + 1) % 60
+                play(["B6:1"], wait=False)
+                increments, offset = False, 0
                 disp_number(minutes * 100 + seconds)
     start = running_time()
-    timer = (minutes * 60 + seconds)
-    left = timer * 1000 + 999
-    while left > 0:
-        minutes, seconds = (left // 1000) // 60, (left // 1000) % 60
+    timer = minutes * 60 + seconds
+    currTime = timer * 1000 + 999
+    stopped = False
+    play(["B6:1"], wait=False)
+    wait_unpressed()
+    print(minutes, seconds)
+    while not stopped:
+        minutes, seconds = (currTime // 1000) // 60, (currTime // 1000) % 60
         disp_number(minutes * 100 + seconds)
         sleep(100)
-        left = timer * 1000 - (running_time() - start) + 999
-    blink(2000)
+        if increments:
+            currTime = ((running_time() - start) + offset +timer*1000) % 1000000
+        else:
+            currTime = timer * 1000 - (running_time() - start) + 999 - offset
+            if currTime <= 0:
+                start, offset, currTime = running_time(), 0, 0 
+                play((["B6:1"]*4 + ["R:2"])*3, wait=False)
+                blink(2000)
+                blink(2000)
+                increments, timer = True, 0
+        if button_a.is_pressed() or button_b.is_pressed():
+            stopped = True
+            play(["B6:1"], wait=False)
+            offset = (running_time()-start) % 1000
+            wait_unpressed()
